7a9c7fd0e5152a3e22d42bca8a7c3ecab27e139a
i7-8700 <1281261856@qq.com>
2022-10-13 06:34:43 +0800

    update

--- 高中数学/code/combination/main.cpp
+++ 高中数学/code/combination/main.cpp
@@ -40,6 +40,21 @@ void p5()
 	printf("%d\n", 3 * combination(8, 3) - 2 * combination(5, 2));
 }
 
+// page27 Ï°Ìâ1.2
+
+void a1()
+{
+	printf("%d\n", 5 * arrangement(5, 3) + 4 * arrangement(4, 2));
+	printf("%d\n", arrangement(4, 1) + arrangement(4, 2) + arrangement(4, 3) + arrangement(4, 4));
+}
+
+void a2()
+{
+	printf("%d\n", combination(15, 3));
+	printf("%d\n", combination(200, 197));
+	printf("%lf\n", 1.0 * combination(6, 3) / combination(8, 4));
+}
+
 int main()
 {
 	assert(combination(1, 0) == 1);
@@ -51,7 +66,10 @@ int main()
 	assert(combination(10, 7) == 120);
 	assert(combination(17, 11) == 12376);
 
-	p1();
-	p2();
-	p5();
+	//p1();
+	//p2();
+	//p5();
+
+	a1();
+	a2();
 }
