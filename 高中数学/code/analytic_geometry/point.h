18d518c6e51273f987aa1ff16d203c7bf3ca0e19
i7-8700 <1281261856@qq.com>
2022-12-09 10:48:23 +0800

    update

--- 高中数学/code/analytic_geometry/point.h
+++ 高中数学/code/analytic_geometry/point.h
@@ -32,6 +32,12 @@ Point<T> middle(const Point<T>& a, const Point<T>& b) {
 	return { (a.x + b.x) / 2, (a.y + b.y) / 2 };
 }
 
+// 三点共线
+template <typename T = int>
+bool threePointsCollinear(const Point<T>& a, const Point<T>& b, const Point<T>& c) {
+	return a.x * b.y + b.x * c.y + c.x * a.y == a.x * c.y + b.x * a.y + c.x * b.y;
+}
+
 
 }
 }
---
+++ 高中数学/code/analytic_geometry/triangle.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "point.h"
+
+namespace math {
+namespace analytic_geometry {
+
+template <typename T = int>
+struct Triangle {
+	Point<T> A, B, C;
+
+	
+};
+
+}
+}
